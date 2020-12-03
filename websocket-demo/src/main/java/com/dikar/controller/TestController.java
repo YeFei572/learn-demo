f1d12ab2b206bdf7905cf09aed681187180e7e10
keppelfei <keppelfei@gmail.com>
2020-12-04 00:36:13 +0800

    update some demo :smile:

---
+++ websocket-demo/src/main/java/com/dikar/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.dikar.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+@Slf4j
+@RestController
+//@RequestMapping(value = "/test")
+@RequiredArgsConstructor
+public class TestController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping(value = "/sendMsg")
+//    @Scheduled(cron = "0/4 * * * * ? ")
+    public Boolean sendMsg(String msg) {
+        log.info("----------------------{}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        simpMessagingTemplate.convertAndSendToUser("1", "/remind", new Random().nextInt(30) + msg);
+        return Boolean.TRUE;
+    }
+}
